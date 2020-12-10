52b50e408ed6060576fb06c27a30bf48694f6eb4
ReAlpha39 <yunusw718@gmail.com>
2020-12-10 22:58:05 +0800

    Add project files.

---
+++ UserRegistrationWebApp/Default.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="UserRegistrationWebApp._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <h1 style="text-align:center">User Registration Form</h1>
+    </div>
+    <div class="container">
+        <asp:Label ID="Label4" runat="server" Text="User ID" style="padding-right: 21px"></asp:Label>
+        <asp:TextBox ID="TextBox3" runat="server" Width="170px"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="Name" style="padding-right: 30px"></asp:Label>
+        <asp:TextBox ID="TextBox1" runat="server" Width="170px"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="Email" style="padding-right: 33px"></asp:Label>
+        <asp:TextBox ID="TextBox2" runat="server" Width="170px"></asp:TextBox>
+        <br />
+        <br />
+        <div   style="padding-left: 33px">
+            <asp:Button ID="Button1" runat="server" Text="Insert" OnClick="Button1_Click"/>
+            <asp:Button ID="Button2" runat="server" Text="Search" OnClick="Button2_Click"/>
+            <asp:Button ID="Button3" runat="server" Text="Update" OnClick="Button3_Click"/>
+            <asp:Button ID="Button4" runat="server" Text="Delete" OnClick="Button4_Click"/>
+        </div>
+        <br />
+        <div style="padding-left: 33px">
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+        </div>
+        <br />
+        <div  style="padding-left: 33px">
+            <asp:Label ID="Label3" runat="server" Text="Label"></asp:Label>
+        </div>
+        <br />
+        
+    </div>
+
+</asp:Content>
---
+++ UserRegistrationWebApp/Default.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using UserRegistrationWebApp.ServiceReference1;
+
+namespace UserRegistrationWebApp
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            InsertUser user = new InsertUser();
+            user.Name = TextBox1.Text;
+            user.Email = TextBox2.Text;
+            string r = client.Insert(user);
+            Label3.Text = r.ToString();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.gettestdata g = new ServiceReference1.gettestdata();
+            g = client.GetInfo();
+            DataTable dataTable = new DataTable();
+            dataTable = g.userTab;
+            GridView1.DataSource = dataTable;
+            GridView1.DataBind();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            UpdateUser updateUser = new UpdateUser();
+            updateUser.UID = int.Parse(TextBox3.Text);
+            updateUser.Name = TextBox1.Text;
+            updateUser.Email = TextBox2.Text;
+            string result = client.Update(updateUser);
+            Label3.Text = result.ToString();
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            DeleteUser deleteUser = new DeleteUser();
+            deleteUser.UID = int.Parse(TextBox3.Text);
+            string res = client.Delete(deleteUser);
+            Label3.Text = res.ToString();
+        }
+    }
+}
